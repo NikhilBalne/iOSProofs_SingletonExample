b4203e02636eae86089ccd399b0fbb2c9f41831e
nikhilbalne <nikhil.b@ecoihub.com>
2020-04-26 13:48:38 +0530

    Initial Commit

---
+++ iOSProofs_SingletonExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSProofs_SingletonExample
+//
+//  Created by Nikhil Balne on 26/04/20.
+//  Copyright © 2020 Nikhil Balne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
